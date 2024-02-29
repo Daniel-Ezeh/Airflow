097f96889b23ff99b4e56db06ec52d44cd5e6b53
“Daniel <danielezeh3@gmail.com>
2024-02-29 15:45:03 +0100

    Added parameters and arguments

--- dags/create_DAG_with_python_operator.py
+++ dags/create_DAG_with_python_operator.py
@@ -10,23 +10,28 @@ def_args = {
 
 
 # Defining a function that want to run in python
-def greet_time():
+# Adding a parameters to the function
+def greet_time(name, age):
     now = datetime.now()
     format = '%H:%M:%S - %d/%m/%Y'
-    print(f"Hello everyone at {datetime.strftime(now, format)}")
+    print(f"Hello everyone at {datetime.strftime(now, format)} \nMy name is {name} and I am {age} years old.")
 
 
 
 with DAG (
     default_args=def_args,
-    dag_id="my_DAG_with_python_operator_v01",
+    dag_id="my_DAG_with_python_operator_v02",
     description="my first DAG using python operator",
     start_date=datetime(2024, 2, 25, 6),
     schedule_interval="@daily"
 ) as DAG:
     task1 = PythonOperator(
         task_id = "greeting_time",
-        python_callable=greet_time
+        python_callable=greet_time,
+        op_kwargs={
+            'name':'Micheal James',
+            'age':19
+        }
     )
 
     task1
