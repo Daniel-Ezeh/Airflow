af4598ed52c425a9024b18340a74296459f506db
“Daniel <danielezeh3@gmail.com>
2024-02-29 15:14:13 +0100

    Working version of the two concurrent task

--- dags/my_first_dag.py
+++ dags/my_first_dag.py
@@ -6,12 +6,12 @@ from airflow.operators.bash import BashOperator
 default_args = {
     'owner':"Dan4sure",
     'retries': 5,
-    'retry_delay': timedelta(minutes=2)
+    'retry_delay': timedelta(seconds=10)
 }
 
 
-with DAG (
-    dag_id = "my_first_dag_v2",
+with DAG(
+    dag_id = "my_first_dag_v4.1.1",
     default_args = default_args,
     description = "this is my first dag I am writing",
     start_date = datetime(2024, 2, 25, 3),
@@ -24,17 +24,20 @@ with DAG (
 
     task2 = BashOperator(
         task_id="second_task",
-        bash_command="echo Hey, I'm the second task and will be running after task 1"
+        bash_command="echo Hey, I am the second task and will be running after task 1"
     )
 
 
     task3 = BashOperator(
         task_id = "third_task",
-        bash_command="echi This is task 3, I will be running concurrently with task 2"
+        bash_command="echo This is task 3, I will be running concurrently with task 2"
     )
     # task1.set_downstream(task2)
     # task1.set_downstream(task3)
 
     # second method
-    task1 >> task2
-    task1 >> task3
+    # task1 >> task2
+    # task1 >> task3
+
+    # third method
+    task1 >> [task2, task3]
